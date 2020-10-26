55b9383e01f7fe88e0985c2c22d588d6faff1d0c
Clément Prod'homme <contact@clementprodhomme.fr>
2020-10-26 15:18:36 +0000

    Implement the logic for the drawer menu

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,1 +1,5 @@
-console.log('Hello world!');
+import A11yDialog from 'a11y-dialog';
+
+const main = document.getElementById('main');
+const drawerDialog = document.getElementById('drawer');
+new A11yDialog(drawerDialog, main);
