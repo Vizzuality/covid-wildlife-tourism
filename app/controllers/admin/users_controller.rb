3f21f63a54410cad2ae5f035b89f3162c2879014
Clément Prod'homme <contact@clementprodhomme.fr>
2020-11-16 11:02:05 +0000

    Let admins create/edit/delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -46,11 +46,10 @@ module Admin
 
       respond_to do |format|
         if @user.save
-          format.html { redirect_to @user, notice: 'User was successfully created.' }
-          format.json { render :show, status: :created, location: @user }
+          format.html { redirect_to admin_user_path(@user), notice: 'User was successfully created.' }
         else
+          flash.now[:alert] = @user.errors.full_messages
           format.html { render :new }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
         end
       end
     end
@@ -61,10 +60,9 @@ module Admin
       respond_to do |format|
         if @user.update(user_params)
           format.html { redirect_to admin_user_path(@user), notice: 'User was successfully updated.' }
-          format.json { render :show, status: :ok, location: @user }
         else
-          format.html { render :edit }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
+          flash.now[:alert] = @user.errors.full_messages
+          format.html { render :edit  }
         end
       end
     end
@@ -74,8 +72,7 @@ module Admin
     def destroy
       @user.destroy
       respond_to do |format|
-        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-        format.json { head :no_content }
+        format.html { redirect_to admin_users_path, notice: 'User was successfully destroyed.' }
       end
     end
 
