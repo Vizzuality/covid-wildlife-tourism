4347e05882de85d1869e5de2431722cc45dff91a
Clément Prod'homme <contact@clementprodhomme.fr>
2020-11-17 15:24:35 +0000

    Fix accessibility issues
    Based on the WebAIM Wave browser extension.

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -13,4 +13,16 @@ module StoresHelper
       'badge-warning'
     end
   end
+
+  def has_previous_value?(attribute)
+    !@store.nil? && !@related_store.nil? && !@store.name.eql?(@related_store[attribute])
+  end
+
+  def conditional_attributes(add_attributes, attributes)
+    if add_attributes
+      attributes
+    else
+      {}
+    end
+  end
 end
