2007c0a1fb6efb0659c193f32651b2e67faa6085
Clément Prod'homme <contact@clementprodhomme.fr>
2020-11-25 11:21:36 +0000

    Fix a bug indicating pin changes when reviewing

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -15,7 +15,7 @@ module StoresHelper
   end
 
   def has_previous_value?(attribute)
-    !@store.nil? && !@related_store.nil? && !@store.name.eql?(@related_store[attribute])
+    !@store.nil? && !@related_store.nil? && !@store[attribute].eql?(@related_store[attribute])
   end
 
   def conditional_attributes(add_attributes, attributes)
